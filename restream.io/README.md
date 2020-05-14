438d5a17306e38e23ca0ac14f8d28af9cd403ea6
Thomas J. Webb <thomas@osakared.com>
2020-05-13 19:13:46 -0700

    Updated for new Franz version and new restream config

--- restream.io/README.md
+++
@@ -1,5 +0,0 @@
-# Zoho for Franz
-This is the unofficial Franz recipe for Restream Titles
-
-### How to create your own Franz recipes:
-* [Read the documentation](https://github.com/meetfranz/plugins)
--- webview.js
+++ webview.js
@@ -1,37 +1,23 @@
 'use strict';
 
 module.exports = Franz => {
-  const getMessages = function getMessages() {
-    let count = 0;
+  let count = 0;
 
-    // Each test is done in order of least accurate (but most robust)
-    // -> most accurate (but least robust)
-    // for reliability of at least getting a result
+  function getMessages() {
+    if (document.getElementsByClassName('favicon-96x96')[0].href.match('circle')) {
+      let newCount = 0;
 
-    // 3rd best test (basic, less accurate but OK if nothing else works)
-    if (document.getElementsByClassName('zA zE').length > 0) {
-      count = document.getElementsByClassName('zA zE').length;
-    }
-
-    if (document.getElementsByClassName('J-Ke n0').length > 0) {
-      // 2nd best (more detailed check, much more accurate if available)
-      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') != null) {
-        count = parseInt(document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label').replace(/[^0-9.]/g, ''), 10);
+      if (document.getElementsByClassName('MuiGrid-root jss231 MuiGrid-item').length > 0) {
+        newCount = document.getElementsByClassName('MuiGrid-root jss231 MuiGrid-item').length;
       }
-
-      // 1st best
-      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('title') != null) {
-        count = parseInt(document.getElementsByClassName('J-Ke n0')[0].getAttribute('title').replace(/[^0-9.]/g, ''), 10);
-      }
+    
+      // set Franz badge
+      Franz.setBadge(newCount - count);
+      count = newCount;
     }
-
-    // Just incase we don't end up with a number, set it back to zero (parseInt can return NaN)
-    if (isNaN(count)) {
-      count = 0;
+    else {
+      Franz.setBadge(0);
     }
-
-    // set Franz badge
-    Franz.setBadge(count);
   };
 
   // check for new messages every second and update Franz badge
